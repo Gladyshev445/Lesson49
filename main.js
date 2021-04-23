f9b4a2ce1fce2eebbcac0cf2a297faa1aeb8654f
Gladyshev445 <74065918+Gladyshev445@users.noreply.github.com>
2021-04-23 17:55:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let a = document.getElementById("text5");
+    
+    // b = a.innerHTML;
+    // a.innerHTML = "Здесь был параграф 3"
+    // b = b + "Салхе"
+    // alert(b);
+    // a.innerHTML = "Ярослав, ты готов"
+    // console.log(a.innerHTML);
+    // // let c = document.getElementsByClassName("Jo");
+    // // console.log(Jo)
+    a.setAttribute("class" , "o")
